9a94486a54efdcd378c0630d9b7e81db57c80758
MarineGaloyan <marine.galoyan.g@tumo.org>
2022-01-29 16:45:21 +0400

    changes_server

--- final/script.js
+++ final/script.js
@@ -1,87 +1,22 @@
+var socket = io();
 
-function generator(matLen, gr, grEat, pred,prEat,fl) {
-    let matrix = [];
-    for (let i = 0; i < matLen; i++) {
-        matrix[i] = [];
-        for (let j = 0; j < matLen; j++) {
-            matrix[i][j] = 0;
-        }
-    }
-    for (let i = 0; i < gr; i++) {
-        let x = Math.floor(Math.random() * matLen);
-        let y = Math.floor(Math.random() * matLen);
-        if (matrix[x][y] == 0) {
-            matrix[x][y] = 1;
-        }
-    }
-    for (let i = 0; i < grEat; i++) {
-        let x = Math.floor(Math.random() * matLen);
-        let y = Math.floor(Math.random() * matLen);
-        if (matrix[x][y] == 0) {
-            matrix[x][y] = 2;
-        }
-    }
-    for (let i = 0; i < pred; i++) {
-        let x = Math.floor(Math.random() * matLen);
-        let y = Math.floor(Math.random() * matLen);
-        if (matrix[x][y] == 0) {
-            matrix[x][y] = 3;
-        }
-    }
-    for (let i = 0; i < prEat; i++) {
-        let x = Math.floor(Math.random() * matLen);
-        let y = Math.floor(Math.random() * matLen);
-        if (matrix[x][y] == 0) {
-            matrix[x][y] = 4;
-        }
-    }
-    for (let i = 0; i < fl; i++) {
-        let x = Math.floor(Math.random() * matLen);
-        let y = Math.floor(Math.random() * matLen);
-        if (matrix[x][y] == 0) {
-            matrix[x][y] = 5;
-        }
-    }
-    return  matrix
-}
 
 let side = 20;
 
-let matrix = generator(20, 25, 5, 5,5,5);
-var grassArr = []
-let grassEaterArr = []
-let predatorArr = []
-let predatoreatArr = []
+
+// var grassArr = []
+// let grassEaterArr = []
+// let predatorArr = []
+// let predatoreatArr = []
+
 function setup() {
     frameRate(5);
-    createCanvas(matrix[0].length * side, matrix.length * side);
+    createCanvas(20* side, 20 * side);
     background('#acacac');
-    for (var y = 0; y < matrix.length; ++y) {
-        for (var x = 0; x < matrix[y].length; ++x) {
-            if (matrix[y][x] == 1) {
-                var gr = new Grass(x, y, 1);
-                grassArr.push(gr);
-            } else if (matrix[y][x] == 2) {
-                var gr = new GrassEater(x, y, 2)
-                grassEaterArr.push(gr)
-            }
-            else if (matrix[y][x] == 3) {
-                var gr = new Predator(x, y, 1)
-                predatorArr.push(gr);
-            }
-            else if (matrix[y][x] == 4) {
-                var gr = new Predatoreat(x, y, 1)
-                predatoreatArr.push(gr);
-            }
-            else if (matrix[y][x] == 8) {
-
-            }
-        }
-    }
-
+  
 
 }
-function draw() {
+function mkarel() {
 
     for (var y = 0; y < matrix.length; y++) {
         for (var x = 0; x < matrix[y].length; x++) {
@@ -112,21 +47,28 @@ function draw() {
         }
 
     }
-    for (var i in grassArr) {
-        grassArr[i].mul();
-    }
-    for (var i in grassEaterArr) {
-        grassEaterArr[i].mul()
-        grassEaterArr[i].eat()
-    }
-    for (var i in predatorArr) {
-        predatorArr[i].mul()
-        predatorArr[i].eat()
-    }
+    setInterval(
+        function () {
+        socket.on('send matrix', nkarel)
+        },1000
+    )
+    
 
-    for (var i in predatoreatArr) {
-        predatoreatArr[i].mul()
-        predatoreatArr[i].eat()
-    }
+    // for (var i in grassArr) {
+    //     grassArr[i].mul();
+    // }
+    // for (var i in grassEaterArr) {
+    //     grassEaterArr[i].mul()
+    //     grassEaterArr[i].eat()
+    // }
+    // for (var i in predatorArr) {
+    //     predatorArr[i].mul()
+    //     predatorArr[i].eat()
+    // }
+
+    // for (var i in predatoreatArr) {
+    //     predatoreatArr[i].mul()
+    //     predatoreatArr[i].eat()
+    // }
 
 }
--- final/server.js
+++ final/server.js
@@ -1,12 +1,126 @@
-var express = require("express");
+var express = require('express');
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var fs = require("fs");
 
 app.use(express.static("."));
 
-app.get("/", function(req, res){
-res.redirect("index.html");
+app.get('/', function (req, res) {
+    res.redirect('index.html');
 });
+server.listen(3000);
 
-app.listen(3000, function(){
-console.log("Example is running on port 3000");
-});
+function generator(matLen, gr, grEat, pred, prEat, fl) {
+    let matrix = [];
+    for (let i = 0; i < matLen; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < matLen; j++) {
+            matrix[i][j] = 0;
+        }
+    }
+    for (let i = 0; i < gr; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 1;
+        }
+    }
+    for (let i = 0; i < grEat; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 2;
+        }
+    }
+    for (let i = 0; i < pred; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 3;
+        }
+    }
+    for (let i = 0; i < prEat; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 4;
+        }
+    }
+    for (let i = 0; i < fl; i++) {
+        let x = Math.floor(Math.random() * matLen);
+        let y = Math.floor(Math.random() * matLen);
+        if (matrix[x][y] == 0) {
+            matrix[x][y] = 5;
+        }
+    }
+    return matrix
+}
+
+let matrix = generator(20, 25, 5, 5, 5, 5);
+io.sockets.emit('send matrix', matrix);
+
+
+grassArr = []
+grassEaterArr = []
+predatorArr = []
+predatoreatArr = []
+
+Grass = require("./grass")
+GrassEater = require("./grasseater")
+Predator = require("./predator")
+PreadatorEat = require("./Predatoreat")
+Flower = require("./flower")
+
+function createObject(matrix) {
+    for (var y = 0; y < matrix.length; ++y) {
+        for (var x = 0; x < matrix[y].length; ++x) {
+            if (matrix[y][x] == 1) {
+                var gr = new Grass(x, y, 1);
+                grassArr.push(gr);
+            } else if (matrix[y][x] == 2) {
+                var gr = new GrassEater(x, y, 2)
+                grassEaterArr.push(gr)
+            }
+            else if (matrix[y][x] == 3) {
+                var gr = new Predator(x, y, 1)
+                predatorArr.push(gr);
+            }
+            else if (matrix[y][x] == 4) {
+                var gr = new Predatoreat(x, y, 1)
+                predatoreatArr.push(gr);
+            }
+            else if (matrix[y][x] == 8) {
+
+            }
+        }
+    }
+}
+io.sockets.emit('send matrix', matrix)
+
+function game (){
+
+for (var i in grassArr) {
+        grassArr[i].mul();
+    }
+    for (var i in grassEaterArr) {
+        grassEaterArr[i].mul()
+        grassEaterArr[i].eat()
+    }
+    for (var i in predatorArr) {
+        predatorArr[i].mul()
+        predatorArr[i].eat()
+    }
+
+    for (var i in predatoreatArr) {
+        predatoreatArr[i].mul()
+        predatoreatArr[i].eat()
+    }
+    io.sockets.emit("send matrix", matrix);
+}
+
+setInterval(game, 1000)
+
+io.on('connection', function () {
+    createObject(matrix)
+})
